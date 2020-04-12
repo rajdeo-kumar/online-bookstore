5018d25fddf0ceff7e1f30c2731855b5c9ddf9d5
Admin <Admin@Admin-PC>
2020-04-12 13:36:35 +0530

    commit first

---
+++ src/main/java/com/springboot/onlinebookstore/OnlineBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookstoreApplication.class, args);
+	}
+
+}
